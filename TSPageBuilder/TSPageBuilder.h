3ebe99c5e6bf04625a41656be9635ac40bb5edea
Jonathan Mitchell <jonathan@mugginsoft.com>
2014-12-23 12:17:34 +0000

    Initial Commit

---
+++ TSPageBuilder/TSPageBuilder.h
@@ -0,0 +1,19 @@
+//
+//  TSPageBuilder.h
+//  TSPageBuilder
+//
+//  Created by Jonathan Mitchell on 23/12/2014.
+//  Copyright (c) 2014 Thesaurus Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TSPageBuilder.
+FOUNDATION_EXPORT double TSPageBuilderVersionNumber;
+
+//! Project version string for TSPageBuilder.
+FOUNDATION_EXPORT const unsigned char TSPageBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSPageBuilder/PublicHeader.h>
+
+
